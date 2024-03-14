3f7f8b4c6166819d3af3082a83a9e50da1cd112d
Carlapretinha <carladias.sp@hotmail.com>
2024-03-14 10:23:51 -0300

    commit

---
+++ app/src/main/java/com/example/agendatarefas/ActivityTarefas.java
@@ -0,0 +1,12 @@
+package com.example.agendatarefas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTarefas extends AppCompatActivity {
+    public void onCreate (Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.layout_activity);
+    }
+}
